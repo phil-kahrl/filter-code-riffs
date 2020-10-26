4eb91cd3f13621d70411a49334cfb9f85a67862e
phil-kahrl <phil.kahrl@viewpath.com>
2020-10-26 16:51:44 -0700

    Fleshing out the examples.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const arccore = require("@encapsule/arccore");
 
-/**
-
-**/
+const filterRequest = {
+    operationID: "6NBRSRutRx2XBc3BIhjzeA",
+    operationName: "filter demo",
+    operationDescription: "filter demo",
+    inputFilterSpec: {
+        ____opaque: true
+    },
+    // outputFilterSpec is optional
+    bodyFunction: (request_) => {
+        console.log("request_ : " + JSON.stringify(request_));
+        return {result: undefined};
+    }
+};
 
 /**
     1. Making arguments optional.
@@ -14,33 +24,40 @@ const arccore = require("@encapsule/arccore");
     1a.  Using "jsUndefined" for ____types or ____accept
 **/
 
-let filterSpec_1a = {
+filterRequest.inputFilterSpec = {
     ____accept: ["jsObject", "jsUndefined"]
 };
+
+arccore.filter.create(filterRequest).result.request();
+// OUTPUT: request_ : undefined
 
 /**
     1b.  Using a ____defaultValue
 **/
 
-let filterSpec_1b = {
+filterRequest.inputFilterSpec = {
     ____accept: "jsObject",
     ____defaultValue: {} // the caller does not need to pass in an argument, but if they do 
                          // it will override the ____defaultValue
 };
+
+arccore.filter.create(filterRequest).result.request();
+// OUTPUT: request_ : {}
 /**
     1c.  Cascading default values.
 **/
 
 // Different ways to do default values.
 
-let filterSpec_1c = {
+filterRequest.inputFilterSpec = {
     ____accept: "jsObject",
     ____defaultValue: {foo: "bar"}
 };
+arccore.filter.create(filterRequest).result.request();
+// OUTPUT: {foo: "bar"}
 
 // is equivalent to:
-
-let filterSpec_1d = {
+filterRequest.inputFilterSpec = {
     ____types: "jsObject", // this needs to be ____types to have sub-namespaces
     ____defaultValue: {},
     foo: {
@@ -48,21 +65,30 @@ let filterSpec_1d = {
         ____defaultValue: "bar"
     }
 };
+arccore.filter.create(filterRequest).result.request();
+// OUTPUT: {foo: "bar"}
 
 // Be careful to avoid pruning on sub-namespaces with ____types
 
-let filterSpec_1e = {
+filterRequest.inputFilterSpec = {
     ____types: "jsObject"
 };
 
-// or 
+arccore.filter.create(filterRequest).result.request({foo: "bar"});
+// The object argument will be pruned to an empty object because there is not sub-namespace under ____types
+// OUTPUT: {}
 
-let filterSpec_1f = {
+// Same thing applies if ____types is an array element spec.
+
+filterRequest.inputFilterSpec = {
     ____types: "jsArray",
     element: {
         ____types: "jsObject"
     }
 };
+
+arccore.filter.create(filterRequest).result.request([{foo: "bar"}]);
+// OUTPUT: [{}]
 
 /**
     2.  Deriving multiple similar filter specs from a common base.
@@ -72,14 +98,13 @@ const baseFilterSpec = {
     id: {
         ____accept: "jsString",
         ____appdsl: {
-            createParam: true
+            requiredForCreate: true
         }
     },
     name: {
         ____accept: "jsString",
         ____appdsl: {
-            editParam: true,
-            createParam: true
+            editable: true,
         }
     }
 };
@@ -94,40 +119,75 @@ const baseFilterSpec = {
 let createSpec = JSON.parse(JSON.stringify(baseFilterSpec)); 
 
 // iterate over the keys
+// anything that is not the create param, will be option (allow undefined) on create.
 Object.keys(baseFilterSpec).forEach( (key) => {
-    if (!createSpec[key].____appdsl.createParam) {
-        delete createSpec[key];
+    if (!createSpec[key].____appdsl.requiredForCreate && (createSpec[key].____appdsl.editable)) {
+        if (createSpec[key].____accept) {
+            if (typeof createSpec[key].____accept === "object") {
+                createSpec[key].____accept.push("jsUndefined");
+            } else {
+                createSpec[key].____accept = [createSpec[key].____accept, "jsUndefined"];
+            }
+        } else if (createSpec[key].____types) {
+            if (typeof createSpec[key].____types === "object") {
+                createSpec[key].____types.push("jsUndefined");
+            } else {
+                createSpec[key].____types = [createSpec[key].____types, "jsUndefined"];
+            }
+        }
     }
+    delete createSpec[key].____appdsl;
 });
 
-let editSpec = {...createSpec}; // make a copy using the spread operator.
+let editSpec = {...baseFilterSpec}; // another way to make a copy using the spread operator.
 
-// similar to iteration of Object.keus
+// similar to iteration of Object.keys
+// delete keys that do not have the editParam appdsl.
 for (const key in editSpec) {
-    const value = editSpec[key];
-    if (!(value.____appdsl.editParam)) {
+    if (!(editSpec[key].____appdsl.editable)) {
         delete editSpec[key];
+    } else {
+        delete editSpec[key].____appdsl;
     }
 }
+
+console.log("baseFilterSpec: " + JSON.stringify(baseFilterSpec));
+console.log("createSpec: " + JSON.stringify(createSpec));
+// createSpec: {"id":{"____accept":"jsString"},"name":{"____accept":["jsString","jsUndefined"]}}
+console.log("editSpec: "  + JSON.stringify(editSpec));
+//editSpec: {"name":{"____accept":"jsString"}}
 
 /**
     3.  Error handling.
 **/
-
-let bodyFunction = (request_) => {
+let bodyFunction = () => {
     try {
-
+        console.log("doing something ...");
     } catch (err) {
-        // Return an error instead of allowing an exception to be thrown.
-        return {error: err.stack}
+        // Always return an error from inside a filter body function instead of allowing an exception to be thrown.
+        return {error: err.stack};
     }
-
-}
+};
 
 /**
     4.  Dissecting a constructed filter.
 **/
 
-/**
-    5.  Factory patterns
-**/
+const factoryResponse = arccore.filter.create(filterRequest);
+
+if (!factoryResponse.error) {
+    // You can see what the request of the constructed filter was by looking at the 'filterDescriptor'
+    console.log("filterDescriptor: " + JSON.stringify(factoryResponse.result.filterDescriptor));
+    // Trying to modify the filterDescriptor to change the filter will not work.
+    factoryResponse.result.filterDescriptor.inputFilterSpec = {____opaque: true}; // has no affect on the constructed filter.
+}
+
+// using ____opaque, means any input will work.
+
+filterRequest.inputFilterSpec = {____opaque: true};
+
+arccore.filter.create(filterRequest).result.request({a: "baz", b: 1, c: {d: true}});
+
+
+
+
